f731c21c4ec6819cf6d271cff2deb0ad50c20679
urubuz <135740667+urubuz@users.noreply.github.com>
2026-04-09 17:45:07 -0300

    Add files via upload

---
+++ aulaadvinhar/script.js
@@ -0,0 +1,8 @@
+var i = Math.floor(Math.random() * 100) + 1;
+let input = parseInt(getElementById("resultado").value);
+
+document.getElementById("resultado").innerHTML = String(i);
+
+if String(input) = i{
+
+}
