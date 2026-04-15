5cdc1fcf5d0d25fd73ace07425a6a7e316313dce
urubuz <135740667+urubuz@users.noreply.github.com>
2026-04-15 15:30:26 -0300

    Delete aulaadvinhar directory

--- aulaadvinhar/script.js
+++
@@ -1,8 +0,0 @@
-var i = Math.floor(Math.random() * 100) + 1;
-let input = parseInt(getElementById("resultado").value);
-
-document.getElementById("resultado").innerHTML = String(i);
-
-if String(input) = i{
-
-}
